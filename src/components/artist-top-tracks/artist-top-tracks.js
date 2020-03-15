101f2d2a71eb42abf6c48b8970212a442114abc4
James De Ocampo <jdeocampo20@gmail.com>
2020-03-15 21:41:39 +1100

    Move properties and functions to the object

--- src/components/artist-top-tracks/artist-top-tracks.js
+++ src/components/artist-top-tracks/artist-top-tracks.js
@@ -1,120 +1,18 @@
-import { ApiKey, PageSize } from '/utils/constants.js';
-
-let topTracksList;
-let totalTrackList;
-let mbid;
-let topTracksCurrentPage;
-let tracks;
-
-let prevButton;
-let nextButton;
-let currentPageElement;
-let artistNameElement;
-let artistImageElement;
-let artistListenersElement;
-
-function createTrackListItem(trackName, playCount, trackImage, rank) {
-    const listElement = document.createElement('li');
-    listElement.innerHTML = `
-        <div class='card track-card'>
-            <h2 class='track-rank'>#${rank}</h2>
-            <div class='track-details'>
-                <h2 class='track-name'>${trackName}</h2>
-                <div class='play-count'>${playCount} plays</div>   
-            </div>
-            <img src='${trackImage}' alt='${trackName} image' class='track-image'>
-        </div>
-    `;
-
-    return listElement;
-}
-
-function updatePaginator(selectedPage) {
-    topTracksCurrentPage = selectedPage;
-    prevButton.disabled = topTracksCurrentPage === 1;
-    nextButton.disabled = tracks.length === totalTrackList;
-
-    currentPageElement.innerText = topTracksCurrentPage;
-    sessionStorage.setItem('topTracksCurrentPage', topTracksCurrentPage);
-}
-
-async function setArtistDetails(urlMbid) {
-    // Get the artist details from the cache
-    let cachedData = sessionStorage.getItem('selectedArtist');
-    const artistDetails = JSON.parse(cachedData);
-
-    artistNameElement.innerText = artistDetails.name;
-    artistImageElement.src = artistDetails.imageUrl;
-    artistListenersElement.innerText = `${artistDetails.listeners} listeners`;
-}
-
-function setTotalTrackList(attr) {
-    totalTrackList = attr.total;
-}
-
-async function getTopTracks(mbid, selectedPage) {
-    try {
-        let data;
-        if (!tracks) {
-            // Get top 50 tracks
-            data = await fetch(
-                `https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&mbid=${mbid}&api_key=${ApiKey}&format=json`
-            );
-
-            const jsonData = await data.json();
-            tracks = jsonData.toptracks.track;
-            setTotalTrackList(jsonData.toptracks['@attr']);
-        }
-
-        const pageStart = PageSize * (selectedPage - 1);
-        const pageEnd = pageStart + PageSize;
-
-        // if the page is more than the data we have
-        // then request new data
-        if (pageEnd > tracks.length) {
-            data = await fetch(
-                `https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&mbid=${mbid}&api_key=${ApiKey}&format=json&limit=${tracks.length +
-                    50}`
-            );
-            const jsonData = await data.json();
-            tracks = jsonData.toptracks.track;
-            setTotalTrackList(jsonData.toptracks['@attr']);
-        }
-
-        topTracksList.innerHTML = '';
-        for (let i = pageStart; i < pageEnd; i++) {
-            const track = tracks[i];
-            const trackImage = track.image[1]['#text'];
-
-            const rank = i + 1;
-
-            topTracksList.appendChild(
-                createTrackListItem(
-                    track.name,
-                    track.playcount,
-                    trackImage,
-                    rank
-                )
-            );
-        }
-
-        await setArtistDetails(mbid);
-        updatePaginator(selectedPage);
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-function next() {
-    getTopTracks(mbid, topTracksCurrentPage + 1);
-}
-
-function prev() {
-    getTopTracks(mbid, topTracksCurrentPage - 1);
-}
+import { ApiKey, PageSize } from '/src/utils/constants.js';
 
 const ArtistTopTracks = {
-    constructor: async () => {
+    topTracksList: null,
+    totalTrackList: null,
+    mbid: null,
+    topTracksCurrentPage: null,
+    tracks: null,
+    prevButton: null,
+    nextButton: null,
+    currentPageElement: null,
+    artistNameElement: null,
+    artistImageElement: null,
+    artistListenersElement: null,
+    constructor: async function() {
         const htmlPage = await fetch(
             'components/artist-top-tracks/artist-top-tracks.html'
         );
@@ -122,36 +20,128 @@ const ArtistTopTracks = {
 
         return html;
     },
-    init: async () => {
-        topTracksCurrentPage = 1;
-        topTracksList = document.getElementById('top-tracks-list');
-        prevButton = document.getElementById('prev-button');
-        nextButton = document.getElementById('next-button');
-        currentPageElement = document.getElementById('current-page');
-        artistNameElement = document.getElementById('artist-name');
-        artistImageElement = document.getElementById('artist-image');
-        artistListenersElement = document.getElementById('artist-listeners');
-
-        prevButton.addEventListener('click', e => {
+    initProperties: function() {
+        this.topTracksList = document.getElementById('top-tracks-list');
+        this.topTracksCurrentPage = 1;
+        this.prevButton = document.getElementById('prev-button');
+        this.nextButton = document.getElementById('next-button');
+        this.currentPageElement = document.getElementById('current-page');
+        this.artistNameElement = document.getElementById('artist-name');
+        this.artistImageElement = document.getElementById('artist-image');
+        this.artistListenersElement = document.getElementById('artist-listeners');
+    },
+    init: async function() {
+        this.initProperties();
+        this.prevButton.addEventListener('click', e => {
             e.preventDefault();
-            prev();
+            this.prev();
         });
-        nextButton.addEventListener('click', e => {
+        this.nextButton.addEventListener('click', e => {
             e.preventDefault();
-            next();
+            this.next();
         });
 
         // get mbid from url
-        const url = location.hash.slice(1).toLowerCase() || '/';
-        const r = url.split('/');
-        mbid = r[2];
+        this.getMbidFromUrl();
 
         // Check session storage for the page
         if (sessionStorage.getItem('topTracksCurrentPage'))
-            topTracksCurrentPage = JSON.parse(
+            this.topTracksCurrentPage = JSON.parse(
                 sessionStorage.getItem('topTracksCurrentPage')
             );
-        getTopTracks(mbid, topTracksCurrentPage);
+        this.getTopTracks(this.mbid, this.topTracksCurrentPage);
+    },
+    getMbidFromUrl: function() {
+        const url = location.hash.slice(1).toLowerCase() || '/';
+        const r = url.split('/');
+        this.mbid = r[2];
+    },
+    getTopTracks: async function(mbid, selectedPage) {
+        try {
+            let data;
+            if (!this.tracks) {
+                // Get top 50 tracks
+                data = await fetch(
+                    `https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&mbid=${mbid}&api_key=${ApiKey}&format=json`
+                );
+    
+                const jsonData = await data.json();
+                this.tracks = jsonData.toptracks.track;
+                this.setTotalTrackList(jsonData.toptracks['@attr']);
+            }
+    
+            const pageStart = PageSize * (selectedPage - 1);
+            const pageEnd = pageStart + PageSize;
+    
+            // if the page is more than the data we have
+            // then request new data
+            if (pageEnd > this.tracks.length) {
+                data = await fetch(
+                    `https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&mbid=${mbid}&api_key=${ApiKey}&format=json&limit=${this.tracks.length +
+                        50}`
+                );
+                const jsonData = await data.json();
+                this.tracks = jsonData.toptracks.track;
+                this.setTotalTrackList(jsonData.toptracks['@attr']);
+            }
+    
+            this.topTracksList.innerHTML = '';
+            for (let i = pageStart; i < pageEnd; i++) {
+                const track = this.tracks[i];
+                const trackImage = track.image[1]['#text'];
+    
+                const rank = i + 1;
+    
+                this.topTracksList.appendChild(
+                    this.createTrackListItem(
+                        track.name,
+                        track.playcount,
+                        trackImage,
+                        rank
+                    )
+                );
+            }
+    
+            await this.setArtistDetails(mbid);
+            this.updatePaginator(selectedPage);
+        } catch (e) {
+            console.error(e);
+        }
+    },
+    setTotalTrackList: function(attr) { this.totalTrackList = attr.total },
+    next: function() { this.getTopTracks(this.mbid, this.topTracksCurrentPage + 1); },  
+    prev: function() { this.getTopTracks(this.mbid, this.topTracksCurrentPage - 1); },
+    updatePaginator: function(selectedPage) {
+        this.topTracksCurrentPage = selectedPage;
+        this.prevButton.disabled = this.topTracksCurrentPage === 1;
+        this.nextButton.disabled = this.tracks.length === this.totalTrackList;
+    
+        this.currentPageElement.innerText = this.topTracksCurrentPage;
+        sessionStorage.setItem('topTracksCurrentPage', this.topTracksCurrentPage);
+    },
+    setArtistDetails: async function() {
+        // Get the artist details from the cache
+        let cachedData = sessionStorage.getItem('selectedArtist');
+        const artistDetails = JSON.parse(cachedData);
+    
+        this.artistNameElement.innerText = artistDetails.name;
+        this.artistImageElement.src = artistDetails.imageUrl;
+        this.artistListenersElement.innerText = `${artistDetails.listeners} listeners`;
+    },
+    createTrackListItem: function(trackName, playCount, trackImage, rank) {
+        const listElement = document.createElement('li');
+        listElement.innerHTML = `
+            <div class='card track-card'>
+                <h2 class='track-rank'>#${rank}</h2>
+                <div class='track-details'>
+                    <h2 class='track-name'>${trackName}</h2>
+                    <div class='play-count'>${playCount} plays</div>   
+                </div>
+                <img src='${trackImage}' alt='${trackName} image' class='track-image'>
+            </div>
+        `;
+    
+        return listElement;
     }
 };
 
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,218 +1,200 @@
 'use strict';
 
-import { ApiKey, PageSize } from '/utils/constants.js';
-
-let topArtistsListElements;
-let countryTitle;
-let limit;
-let totalArtistsCount;
-let topArtistsList;
-
-let prevButton;
-let nextButton;
-let currentPageElement;
-
-let topArtistsCurrentPage;
-let currentCountry;
-let totalPages;
-
-function createArtistListItem(
-    artistName,
-    artistImage,
-    artistListeners,
-    rank,
-    artistMbid
-) {
-    const listElement = document.createElement('li');
-    listElement.innerHTML = `
-        <div class='card artist-details-card'>
-            <h2 class='artist-rank'>#${rank}</h2>
-            <div class='artist-details'>
-                <h2>${artistName}</h2>
-                <div class='listeners'>${artistListeners} listeners</div>   
-            </div>
-            <a href='#/artists/${artistMbid}' class='artist-image-link'>
-                <img src='${artistImage}' alt='${artistName} image' class='artist-image'>
-            </a>
-        </div>
-    `;
-
-    return listElement;
-}
-
-function setCountryTitle() {
-    countryTitle.innerText = currentCountry;
-}
-
-function addOnClickListener(
-    artistListItem,
-    artistMbid,
-    artistName,
-    artistImageUrl,
-    artistListeners
-) {
-    const artistDetails = {
-        mbid: artistMbid,
-        name: artistName,
-        imageUrl: artistImageUrl,
-        listeners: artistListeners
-    };
-    artistListItem.addEventListener('click', () => {
-        sessionStorage.setItem('selectedArtist', JSON.stringify(artistDetails));
-    });
-}
-
-function setTotalArtistCount(attr) {
-    totalArtistsCount = attr.total;
-}
-
-// get the data from the json file
-async function getArtists(country, selectedPage) {
-    try {
-        currentCountry = country;
-        // cache the selected country
-        sessionStorage.setItem('selectedCountry', JSON.stringify(country));
-
-        let data;
-
-        if (!topArtistsList) {
-            // Get top 50 artists
-            data = await fetch(
-                `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${ApiKey}&format=json&limit=${limit}`
-            );
-
-            const jsonData = await data.json();
-            topArtistsList = jsonData.topartists.artist;
-            setTotalArtistCount(jsonData.topartists['@attr']);
-        }
-
-        const pageStart = PageSize * (selectedPage - 1);
-        const pageEnd = pageStart + PageSize;
-
-        // if the page is more than the data we have
-        // then request new data
-        if (pageEnd > topArtistsList.length) {
-            // fetch the next 50 results
-            const apiPage = (topArtistsList.length + limit) / 50;
-            data = await fetch(
-                `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${ApiKey}&format=json&limit=${limit}&page=${apiPage}`
-            );
-            const jsonData = await data.json();
-            topArtistsList = topArtistsList.concat(jsonData.topartists.artist);
-            setTotalArtistCount(jsonData.topartists['@attr']);
-        }
-
-        // Reset the list to blank
-        topArtistsListElements.innerHTML = '';
-
-        // Create a list item for each artists
-        for (let i = pageStart; i < pageEnd; i++) {
-            // Get artist
-            const artist = topArtistsList[i];
-            // Get the medium image
-            const artistImage = artist.image[1]['#text'];
-
-            const rank = i + 1;
-
-            const artistListItem = createArtistListItem(
-                artist.name,
-                artistImage,
-                artist.listeners,
-                rank,
-                artist.mbid
-            );
-            topArtistsListElements.appendChild(artistListItem);
-
-            // Save the large image to the storage
-            addOnClickListener(
-                artistListItem,
-                artist.mbid,
-                artist.name,
-                artist.image[2]['#text'],
-                artist.listeners
-            );
-        }
-
-        setCountryTitle(country);
-        updatePaginator(selectedPage);
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-function next() {
-    getArtists(currentCountry, topArtistsCurrentPage + 1);
-}
-
-function prev() {
-    getArtists(currentCountry, topArtistsCurrentPage - 1);
-}
-
-function updatePaginator(selectedPage) {
-    topArtistsCurrentPage = selectedPage;
-    prevButton.disabled = topArtistsCurrentPage === 1;
-    nextButton.disabled = topArtistsCurrentPage === totalPages;
-
-    currentPageElement.innerText = topArtistsCurrentPage;
-    sessionStorage.setItem('topArtistsCurrentPage', topArtistsCurrentPage);
-}
+import { ApiKey, PageSize } from '/src/utils/constants.js';
 
 const Home = {
-    constructor: async () => {
+    topArtistsListElements: null,
+    countryTitle: null,
+    limit: null,
+    totalArtistsCount: null,
+    topArtistsList: null,
+    prevButton: null,
+    nextButton: null,
+    currentPageElement: null,
+    topArtistsCurrentPage: null,
+    currentCountry: null,
+    totalPages: null,
+    constructor: async function() {
         // Get the view
         const htmlPage = await fetch('components/home/home.html');
         const html = await htmlPage.text();
 
         return html;
     },
-    init: async () => {
-        topArtistsListElements = document.getElementById('top-artists-list');
-        countryTitle = document.getElementById('country-title');
+    initProperties: function() {
+        this.topArtistsListElements = document.getElementById('top-artists-list');
+        this.countryTitle = document.getElementById('country-title');
+        this.limit = 50;
+        this.prevButton = document.getElementById('prev-button');
+        this.nextButton = document.getElementById('next-button');
+        this.currentPageElement = document.getElementById('current-page');
 
-        prevButton = document.getElementById('prev-button');
-        nextButton = document.getElementById('next-button');
-        currentPageElement = document.getElementById('current-page');
+        this.topArtistsCurrentPage = 1;
+        this.currentCountry = 'Australia';
+    },
+    init: async function() {
+        this.initProperties();
+        this.prevButton.addEventListener('click', e => {
+            e.preventDefault();
+            this.prev();
+        });
+        this.nextButton.addEventListener('click', e => {
+            e.preventDefault();
+            this.next();
+        });
 
-        prevButton.addEventListener('click', e => {
-            e.preventDefault();
-            prev();
-        });
-        nextButton.addEventListener('click', e => {
-            e.preventDefault();
-            next();
-        });
         document
             .getElementById('search-button')
             .addEventListener('click', e => {
                 e.preventDefault();
                 const country = document.querySelector('[name="countryInput"]')
                     .value;
-                limit = 50;
-                topArtistsList = null;
-                getArtists(country, 1);
+                this.topArtistsList = null;
+                this.getArtists(country, 1);
             });
-
-        topArtistsCurrentPage = 1;
-        limit = 50;
-        currentCountry = 'Australia';
-        totalPages;
 
         // Check session storage for the page
         if (sessionStorage.getItem('topArtistsCurrentPage'))
-            topArtistsCurrentPage = JSON.parse(
+        this.topArtistsCurrentPage = JSON.parse(
                 sessionStorage.getItem('topArtistsCurrentPage')
             );
 
         // Check session storage for the page
         if (sessionStorage.getItem('selectedCountry'))
-            currentCountry = JSON.parse(
+            this.currentCountry = JSON.parse(
                 sessionStorage.getItem('selectedCountry')
             );
 
-        getArtists(currentCountry, topArtistsCurrentPage);
+            this.getArtists(this.currentCountry, this.topArtistsCurrentPage);
 
         // Reset the page for the artists top tracks
         sessionStorage.removeItem('topTracksCurrentPage');
+    },
+    next: function() { this.getArtists(this.currentCountry, this.topArtistsCurrentPage + 1); },
+    prev: function() { this.getArtists(this.currentCountry, this.topArtistsCurrentPage - 1); },
+    updatePaginator: function(selectedPage) {
+        this.topArtistsCurrentPage = selectedPage;
+        this.prevButton.disabled = this.topArtistsCurrentPage === 1;
+        this.nextButton.disabled = this.topArtistsCurrentPage === this.totalPages;
+
+        this.currentPageElement.innerText = this.topArtistsCurrentPage;
+        sessionStorage.setItem('topArtistsCurrentPage', this.topArtistsCurrentPage);
+    },
+    getArtists: async function(country, selectedPage) {
+        try {
+            this.currentCountry = country;
+            // cache the selected country
+            sessionStorage.setItem('selectedCountry', JSON.stringify(country));
+    
+            let data;
+    
+            if (!this.topArtistsList) {
+                // Get top 50 artists
+                data = await fetch(
+                    `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${ApiKey}&format=json&limit=${this.limit}`
+                );
+    
+                const jsonData = await data.json();
+                this.topArtistsList = jsonData.topartists.artist;
+                this.setTotalArtistCount(jsonData.topartists['@attr']);
+            }
+    
+            const pageStart = PageSize * (selectedPage - 1);
+            const pageEnd = pageStart + PageSize;
+    
+            // if the page is more than the data we have
+            // then request new data
+            if (pageEnd > this.topArtistsList.length) {
+                // fetch the next 50 results
+                const apiPage = (this.topArtistsList.length + this.limit) / 50;
+                data = await fetch(
+                    `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${ApiKey}&format=json&limit=${this.limit}&page=${apiPage}`
+                );
+                const jsonData = await data.json();
+                this.topArtistsList = this.topArtistsList.concat(jsonData.topartists.artist);
+                this.setTotalArtistCount(jsonData.topartists['@attr']);
+            }
+    
+            // Reset the list to blank
+            this.topArtistsListElements.innerHTML = '';
+    
+            // Create a list item for each artists
+            for (let i = pageStart; i < pageEnd; i++) {
+                // Get artist
+                const artist = this.topArtistsList[i];
+                // Get the medium image
+                const artistImage = artist.image[1]['#text'];
+    
+                const rank = i + 1;
+    
+                const artistListItem = this.createArtistListItem(
+                    artist.name,
+                    artistImage,
+                    artist.listeners,
+                    rank,
+                    artist.mbid
+                );
+                this.topArtistsListElements.appendChild(artistListItem);
+    
+                // Save the large image to the storage
+                this.addOnClickListener(
+                    artistListItem,
+                    artist.mbid,
+                    artist.name,
+                    artist.image[2]['#text'],
+                    artist.listeners
+                );
+            }
+    
+            this.setCountryTitle(country);
+            this.updatePaginator(selectedPage);
+        } catch (e) {
+            console.error(e);
+        }
+    },
+    setTotalArtistCount: function(attr) {
+        this.totalArtistsCount = attr.total;
+    },
+    addOnClickListener: function(
+        artistListItem,
+        artistMbid,
+        artistName,
+        artistImageUrl,
+        artistListeners
+    ) {
+        const artistDetails = {
+            mbid: artistMbid,
+            name: artistName,
+            imageUrl: artistImageUrl,
+            listeners: artistListeners
+        };
+        artistListItem.addEventListener('click', () => {
+            sessionStorage.setItem('selectedArtist', JSON.stringify(artistDetails));
+        });
+    },
+    setCountryTitle: function() { this.countryTitle.innerText = this.currentCountry; },
+    createArtistListItem: function(
+        artistName,
+        artistImage,
+        artistListeners,
+        rank,
+        artistMbid
+    ) {
+        const listElement = document.createElement('li');
+        listElement.innerHTML = `
+            <div class='card artist-details-card'>
+                <h2 class='artist-rank'>#${rank}</h2>
+                <div class='artist-details'>
+                    <h2>${artistName}</h2>
+                    <div class='listeners'>${artistListeners} listeners</div>   
+                </div>
+                <a href='#/artists/${artistMbid}' class='artist-image-link'>
+                    <img src='${artistImage}' alt='${artistName} image' class='artist-image'>
+                </a>
+            </div>
+        `;
+    
+        return listElement;
     }
 };
 
